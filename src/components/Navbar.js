c32c94aac66df29e9f985df3d79179d38cc0010e
Kavya Hegde <kavyahegde2005@gmail.com>
2026-03-08 11:57:42 +0530

    new

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,14 +8,14 @@ export default class Navbar extends Component {
       <div>
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
   <div className="container-fluid">
-    <a className="navbar-brand" href="/">{this.props.title}</a>
+    <Link className="navbar-brand" to="/">{this.props.title}</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarNav">
       <ul className="navbar-nav">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/news">Home</a>
+          <Link className="nav-link active" aria-current="page" to="/news">Home</Link>
         </li>
         <li className="nav-item">
           <Link className="nav-link" to="/tech">Tech</Link>
